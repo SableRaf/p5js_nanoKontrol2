21312e651648f9203c2ededcc9124097223d13ec
Raphaël de Courville <groupes.raphael@gmail.com>
2026-06-26 20:40:12 +0200

    Improve helloButton example

--- examples/basic/helloButton/sketch.js
+++ examples/basic/helloButton/sketch.js
@@ -1,21 +1,45 @@
-// Press the Play or Stop button on the nanoKONTROL2 
+// Press the PLAY or STOP button on the nanoKONTROL2 
 // to change the background color of the canvas
 
 let midi;
 
+let caption = 'Press PLAY or STOP on the nanoKONTROL2';
+
+const playColor = "#96df96";
+const stopColor = "#db6074";
+
 function setup() {
   createCanvas(600, 600);
+  
   midi = new NanoKontrol2();
+
   background(176);
+  drawCaption(caption);
+}
+
+function drawCaption(s){
+  push();
+  textAlign(CENTER, CENTER);
+  textSize(16);
+  translate(width/2, height/2);
+  text(s, 0, 0);
+  pop();
 }
 
 function buttonPressed() {
-  if (midi.input.name === 'PLAY') {
-    background("lime");
-    console.log('Play button pressed');
+  const { name } = midi.input;
+  
+  if (name === 'PLAY') {
+    console.log('PLAY button pressed');
+    caption = 'PLAY button pressed!';
+    background(playColor);
   }
-  if (midi.input.name === 'STOP') {
-    background("red");
-    console.log('Stop button pressed');
+  
+  if (name === 'STOP') {
+    console.log('STOP button pressed');
+    caption = 'STOP button pressed!';
+    background(stopColor);
   }
+
+  drawCaption(caption);
 }
